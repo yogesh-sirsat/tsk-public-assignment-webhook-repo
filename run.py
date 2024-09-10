70a1f87342ee0c7e0df9086c727d7e0dabf3c1c2
Yogesh Sirsat <yogeshsirsat56@gmail.com>
2024-09-11 01:33:34 +0530

    Set flask debug status based on ENV.

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()  # This loads the variables from .env
 app = create_app()
 
-if __name__ == '__main__': 
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Set debug status based on ENV
+    app.run(debug=os.getenv("FLASK_ENV", "development") == "development")
